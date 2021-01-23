90178ddaf62659687e9d421cf1b7260864f72fcf
aminazubenko <71377896+aminazubenko@users.noreply.github.com>
2021-01-23 04:17:18 +0300

    Реализованы функции

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,6 +1,5 @@
 import typing as tp
 
-
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     Encrypts plaintext using a Caesar cipher.
@@ -15,7 +14,19 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for i in plaintext:
+        if (ord(i) >= ord("a")) and (ord(i) <= ord("z")):
+            if (ord(i) + shift) > ord("z"):
+                ciphertext += chr((ord(i) + shift) - 26)
+            else:
+                ciphertext += chr(ord(i) + shift)
+        elif (ord(i) >= ord("A")) and (ord(i) <= ord("Z")):
+            if (ord(i) + shift) > ord("Z"):
+                ciphertext += chr((ord(i) + shift) - 26)
+            else:
+                ciphertext += chr(ord(i) + shift)
+        else:
+            ciphertext += i
     return ciphertext
 
 
@@ -33,7 +44,19 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for i in ciphertext:
+        if (ord(i) >= ord("a")) and (ord(i) <= ord("z")):
+            if (ord(i) - shift) < ord("a"):
+                plaintext += chr((ord(i) - shift) + 26)
+            else:
+                plaintext += chr(ord(i) - shift)
+        elif (ord(i) >= ord("A")) and (ord(i) <= ord("Z")):
+            if (ord(i) - shift) < ord("A"):
+                plaintext += chr((ord(i) - shift) + 26)
+            else:
+                plaintext += chr(ord(i) - shift)
+        else:
+            plaintext = plaintext + i
     return plaintext
 
 
