2892ea23e5f50427b4bf1e69b2d1d8510420e93e
aminazubenko <71377896+aminazubenko@users.noreply.github.com>
2021-01-23 04:36:47 +0300

    Реализованы функции

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -10,7 +10,18 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for i in range(len(plaintext)):
+        x = plaintext[i]
+        y = keyword[i % len(keyword)]
+        if ("a" <= x <= "z"):
+            key = (ord(x) + ord(y)) - ord("a")
+            if (key > ord("z")):
+                key -= 26
+        else:
+            key = (ord(x) + ord(y)) - ord("A")
+            if (key > ord("Z")):
+                key -= 26
+        ciphertext += chr(key)
     return ciphertext
 
 
@@ -26,5 +37,16 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for i in range(len(ciphertext)):
+        x = ciphertext[i]
+        y = keyword[i % len(keyword)]
+        if ("a" <= x <= "z"):
+            key = (ord(x) - ord(y)) + ord("a")
+            if (key < ord("a")):
+                key += 26
+        else:
+            key = (ord(x) - ord(y)) + ord("A")
+            if (key < ord("A")):
+                key += 26
+        plaintext += chr(key)
     return plaintext
