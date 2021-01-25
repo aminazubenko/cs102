8f658ae1682a90c1c1c78789751b7f957b69fe98
aminazubenko <amina.zubenko1@gmail.com>
2021-01-25 23:49:51 +0300

    Добавлено решение для функций

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -10,7 +10,27 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    first_small = ord("a")
+    last_small = ord("z")
+    first_big = ord("A")
+    last_big = ord("Z")
+    number = 26
+    for i, letter in enumerate(plaintext):
+        shift = ord((keyword[i % len(keyword)]).lower()) - first_small
+        if letter.isalpha() and shift != 0:
+            chr_i = ord(letter)
+            if first_small <= chr_i <= last_small:
+                chr_i += shift
+                if chr_i > last_small:
+                    chr_i -= number
+                ciphertext += chr(chr_i)
+            elif first_big <= chr_i <= last_big:
+                chr_i += shift
+                if chr_i > last_big:
+                    chr_i -= number
+                ciphertext += chr(chr_i)
+        else:
+            ciphertext += letter
     return ciphertext
 
 
@@ -26,5 +46,25 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    first_small = ord("a")
+    last_small = ord("z")
+    first_big = ord("A")
+    last_big = ord("Z")
+    number = 26
+    for i, letter in enumerate(ciphertext):
+        shift = ord((keyword[i % len(keyword)]).lower()) - first_small
+        if letter.isalpha() and shift != 0:
+            chr_i = ord(letter)
+            if first_small <= chr_i <= last_small:
+                chr_i -= shift
+                if chr_i < first_small:
+                    chr_i += number
+                plaintext += chr(chr_i)
+            elif first_big <= chr_i <= last_big:
+                chr_i -= shift
+                if chr_i < first_big:
+                    chr_i += number
+                plaintext += chr(chr_i)
+        else:
+            plaintext += letter
     return plaintext
